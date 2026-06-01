c60e155c0b06963c246f7f564a084a91d638a561
PixelRican <rimercado02@gmail.com>
2026-06-01 16:31:02 -0400

    Move classes to their own files

---
+++ Scripts/api.py
@@ -0,0 +1,318 @@
+﻿from typing import Any
+
+class Extension:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.header: Header = Header(data["header"])
+        self.builtin_class_sizes: list[BuiltinClassSize] = [
+            BuiltinClassSize(element) for element in data["builtin_class_sizes"]
+        ]
+        self.builtin_class_member_offsets: list[BuiltinClassMemberOffset] = [
+            BuiltinClassMemberOffset(element) for element in data["builtin_class_member_offsets"]
+        ]
+        self.global_constants: list = data["global_constants"]
+        self.global_enums: list[GlobalEnum] = [
+            GlobalEnum(element) for element in data["global_enums"]
+        ]
+        self.utility_functions: list[UtilityFunction] = [
+            UtilityFunction(element) for element in data["utility_functions"]
+        ]
+        self.builtin_classes: list[BuiltinClass] = [
+            BuiltinClass(element) for element in data["builtin_classes"]
+        ]
+        self.classes: list[Class] = [
+            Class(element) for element in data["classes"]
+        ]
+        self.singletons: list[Singleton] = [
+            Singleton(element) for element in data["singletons"]
+        ]
+        self.native_structures: list[NativeStructure] = [
+            NativeStructure(element) for element in data["native_structures"]
+        ]
+
+class Header:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.version_major: int = data["version_major"]
+        self.version_minor: int = data["version_minor"]
+        self.version_patch: int = data["version_patch"]
+        self.version_status: str = data["version_status"]
+        self.version_build: str = data["version_build"]
+        self.version_full_name: str = data["version_full_name"]
+        self.precision: str = data["precision"]
+
+class BuiltinClassSize:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.build_configuration: str = data["build_configuration"]
+        self.sizes: list[BuiltinClassSizeRecord] = [
+            BuiltinClassSizeRecord(element) for element in data["sizes"]
+        ]
+
+class BuiltinClassSizeRecord:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.size: int = data["size"]
+
+class BuiltinClassMemberOffset:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.build_configuration: str = data["build_configuration"]
+        self.classes: list[BuiltinClassMemberOffsetGrouping] = [
+            BuiltinClassMemberOffsetGrouping(element) for element in data["classes"]
+        ]
+
+class BuiltinClassMemberOffsetGrouping:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.members: list[BuiltinClassMemberOffsetRecord] = [
+            BuiltinClassMemberOffsetRecord(element) for element in data["members"]
+        ]
+
+class BuiltinClassMemberOffsetRecord:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.member: str = data["member"]
+        self.offset: int = data["offset"]
+        self.meta: str = data["meta"]
+
+class GlobalEnum:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.is_bitfield: bool = data["is_bitfield"]
+        self.values: list[GlobalEnumValue] = [
+            GlobalEnumValue(element) for element in data["values"]
+        ]
+
+class GlobalEnumValue:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.value: int = data["value"]
+
+class UtilityFunction:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.return_type: str | None = data.get("return_type")
+        self.category: str = data["category"]
+        self.is_vararg: bool = data["is_vararg"]
+        self.hash: int = data["hash"]
+        self.arguments: list[UtilityFunctionArgument] | None = None
+        arguments: list[dict[str, Any]] | None = data.get("arguments")
+        if arguments:
+            self.arguments = [
+                UtilityFunctionArgument(element) for element in arguments
+            ]
+
+class UtilityFunctionArgument:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+
+class BuiltinClass:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.is_keyed: bool = data["is_keyed"]
+        self.operators: list[BuiltinClassOperator] = [
+            BuiltinClassOperator(element) for element in data["operators"]
+        ]
+        self.constructors: list[BuiltinClassConstructor] = [
+            BuiltinClassConstructor(element) for element in data["constructors"]
+        ]
+        self.has_destructor: bool = data["has_destructor"]
+        self.indexing_return_type: str | None = data.get("indexing_return_type")
+        self.methods: list[BuiltinClassMethod] | None = None
+        self.constants: list[BuiltinClassConstant] | None = None
+        self.enums: list[BuiltinClassEnum] | None = None
+        methods: list[dict[str, Any]] | None = data.get("methods")
+        constants: list[dict[str, Any]] | None = data.get("constants")
+        enums: list[dict[str, Any]] | None = data.get("enums")
+        if methods:
+            self.methods = [
+                BuiltinClassMethod(element) for element in methods
+            ]
+        if constants:
+            self.constants = [
+                BuiltinClassConstant(element) for element in constants
+            ]
+        if enums:
+            self.enums = [
+                BuiltinClassEnum(element) for element in enums
+            ]
+
+class BuiltinClassOperator:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.right_type: str | None = data.get("right_type")
+        self.return_type: str = data["return_type"]
+
+class BuiltinClassConstructor:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.index: int = data["index"]
+        self.arguments: list[BuiltinClassConstructorArgument] | None = None
+        arguments: list[dict[str, Any]] | None = data.get("arguments")
+        if arguments:
+            self.arguments = [
+                BuiltinClassConstructorArgument(element) for element in arguments
+            ]
+
+class BuiltinClassConstructorArgument:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+
+class BuiltinClassMethod:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.return_type: str | None = data.get("return_type")
+        self.is_vararg: bool = data["is_vararg"]
+        self.is_const: bool = data["is_const"]
+        self.is_static: bool = data["is_static"]
+        self.hash: int = data["hash"]
+        self.arguments: list | None = None
+        self.hash_compatibility: int = data["hash"]
+        arguments: list[dict[str, Any]] | None = data.get("arguments")
+        if arguments:
+            self.arguments = [
+                BuiltinClassMethodArgument(element) for element in arguments
+            ]
+
+class BuiltinClassMethodArgument:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+        self.default_value: str | float | int | bool | None = data.get("default_value")
+
+class BuiltinClassConstant:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+        self.value: str = data["value"]
+
+class BuiltinClassEnum:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.values: list[BuiltinClassEnumValue] = [
+            BuiltinClassEnumValue(element) for element in data["values"]
+        ]
+
+class BuiltinClassEnumValue:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.value: int = data["value"]
+
+class Class:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.is_refcounted: bool = data["is_refcounted"]
+        self.is_instantiable: bool = data["is_instantiable"]
+        self.inherits: str | None = data.get("inherits")
+        self.api_type: str = data["api_type"]
+        self.enums: list[ClassEnum] | None = None
+        self.methods: list[ClassMethod] | None = None
+        self.properties: list[ClassProperty] | None = None
+        self.signals: list[ClassSignal] | None = None
+        self.constants: list[ClassConstant] | None = None
+        enums: list[dict[str, Any]] | None = data.get("enums")
+        methods: list[dict[str, Any]] | None = data.get("methods")
+        properties: list[dict[str, Any]] | None = data.get("properties")
+        signals: list[dict[str, Any]] | None = data.get("signals")
+        constants: list[dict[str, Any]] | None = data.get("constants")
+        if enums:
+            self.enums = [
+                ClassEnum(element) for element in enums
+            ]
+        if methods:
+            self.methods = [
+                ClassMethod(element) for element in methods
+            ]
+        if properties:
+            self.properties = [
+                ClassProperty(element) for element in properties
+            ]
+        if signals:
+            self.signals = [
+                ClassSignal(element) for element in signals
+            ]
+        if constants:
+            self.constants = [
+                ClassConstant(element) for element in constants
+            ]
+
+class ClassEnum:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.values: list[ClassEnumValue] = [
+            ClassEnumValue(element) for element in data["values"]
+        ]
+        self.is_bitfield: bool = data["is_bitfield"]
+
+class ClassEnumValue:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.value: int = data["value"]
+
+class ClassMethod:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.is_const: bool = data["is_const"]
+        self.is_vararg: bool = data["is_vararg"]
+        self.is_static: bool = data["is_static"]
+        self.is_virtual: bool = data["is_virtual"]
+        self.hash: int = data["hash"]
+        self.hash_compatibility: list[int] | None = data.get("hash_compatibility")
+        self.return_value: ClassMethodReturnValue | None = None
+        self.arguments: list[ClassMethodArgument] | None = None
+        self.is_required: bool | None = data.get("is_required")
+        return_value: dict[str, Any] | None = data.get("return_value")
+        arguments: list[dict[str, Any]] | None = data.get("arguments")
+        if return_value:
+            self.return_value = ClassMethodReturnValue(return_value)
+        if arguments:
+            self.arguments = [
+                ClassMethodArgument(element) for element in arguments
+            ]
+
+class ClassMethodReturnValue:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.type: str = data["type"]
+        self.meta: str | None = data.get("meta")
+
+class ClassMethodArgument:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+        self.default_value: str | float | int | bool | None = data.get("default_value")
+        self.meta: str | None = data.get("meta")
+
+class ClassProperty:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+        self.setter: str | None = data.get("setter")
+        self.getter: str | None = data.get("getter")
+        self.index: int | None = data.get("index")
+
+class ClassSignal:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.arguments: list[ClassSignalArgument] | None = None
+        arguments: list[dict[str, Any]] | None = data.get("arguments")
+        if arguments:
+            self.arguments = [
+                ClassSignalArgument(element) for element in arguments
+            ]
+
+class ClassSignalArgument:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+
+class ClassConstant:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.value: int = data["value"]
+
+class Singleton:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+
+class NativeStructure:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.format: str = data["format"]
---
+++ Scripts/gdextension.py
@@ -0,0 +1,257 @@
+﻿from itertools import chain
+from typing import Any, Iterator
+
+class Interface:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.copyright: list[str] = data["_copyright"]
+        self.schema: str = data["$schema"]
+        self.format_version: int = data["format_version"]
+        self.types: list[Type] = []
+        self.interface: list[InterfaceFunction] = []
+        for type_data in data["types"]:
+            name: str = type_data["name"]
+            kind: str = type_data["kind"]
+            match kind:
+                case "enum":
+                    self.types.append(Enum(type_data))
+                case "handle":
+                    self.types.append(Handle(type_data))
+                case "alias":
+                    self.types.append(Alias(type_data))
+                case "struct":
+                    self.types.append(Struct(type_data))
+                case "function":
+                    self.types.append(Function(type_data))
+                case _:
+                    raise ValueError(f"'{name}' has invalid kind '{kind}.'")
+        for interface_data in data["interface"]:
+            self.interface.append(InterfaceFunction(interface_data))
+
+    def generate(self) -> Iterator[str]:
+        types: dict[str, Type] = {}
+        global_section: list[Type] = []
+        local_section: list[Type] = []
+        for element in self.types:
+            types[element.name] = element
+            match element.kind:
+                case "handle" | "alias" | "function":
+                    global_section.append(element)
+                case "enum" | "struct":
+                    local_section.append(element)
+        for line in self.copyright:
+            yield line + "\n"
+        yield "\n"
+        for element in chain(global_section, self.interface):
+            for line in element.generate(types):
+                yield line
+        yield "\n"
+        yield "using System;\n"
+        yield "using System.Runtime.InteropServices;\n"
+        yield "\n"
+        yield "namespace Godot;\n"
+        for element in local_section:
+            yield "\n"
+            for line in element.generate(types):
+                yield line
+
+class Type:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.kind: str | None  = data.get("kind")
+        self.description: list[str] | None = data.get("description")
+        self.deprecated: Deprecated | None = None
+        deprecated: dict[str, Any] | None = data.get("deprecated")
+        if deprecated:
+            self.deprecated = Deprecated(deprecated)
+
+    def expand(self, types: dict[str, Type]) -> str:
+        raise NotImplementedError()
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        raise NotImplementedError()
+
+class Deprecated:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.since: str = data["since"]
+        self.message: str | None = data.get("message")
+        self.replacement: str | None = data.get("replacement")
+
+class Enum(Type):
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
+        self.values: list[EnumValue] = [
+            EnumValue(value) for value in data["values"]
+        ]
+        self.is_bitfield: bool | None = data.get("is_bitfield")
+
+    def expand(self, types: dict[str, Enum]) -> str:
+        return "Godot." + self.name
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        if self.is_bitfield:
+            yield "[Flags]\n"
+        yield f"public enum {self.name}\n"
+        yield "{\n"
+        for member in self.values:
+            member_name: str = member.name.title().replace("Gde", "GDE", 1).replace("_", "")
+            member_value: int = member.value
+            yield f"    {member_name} = {member_value},\n"
+        yield "}\n"
+
+class EnumValue:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.value: int = data["value"]
+        self.description: list[str] | None = data.get("description")
+
+class Handle(Type):
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
+        self.parent: str | None = data.get("parent")
+        self.is_const: bool | None = data.get("is_const")
+        self.is_uninitialized: bool | None = data.get("is_uninitialized")
+
+    def expand(self, types: dict[str, Type]) -> str:
+        return "void*"
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        yield f"global using unsafe {self.name} = void*;\n"
+
+class Alias(Type):
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
+        self.type: str = data["type"]
+
+    def expand(self, types: dict[str, Type]) -> str:
+        typedef: Type | None = types.get(self.type)
+        if typedef:
+            return typedef.expand(types)
+        alias_type, _, _ = resolve(self.type)
+        return alias_type
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        yield f"global using {self.name} = {self.expand(types)};\n"
+
+class Struct(Type):
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
+        self.members: list[StructMember] = [
+            StructMember(member) for member in data["members"]
+        ]
+
+    def expand(self, types: dict[str, Type]) -> str:
+        return "Godot." + self.name
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        yield "[StructLayout(LayoutKind.Sequential)]\n"
+        yield f"public struct {self.name}\n"
+        yield "{\n"
+        for member in self.members:
+            member_name: str = member.name.title().replace("_", "")
+            member_type, is_readonly, is_unsafe = resolve(member.type)
+            modifiers: list[str] = ["public"]
+            if is_readonly:
+                modifiers.append("readonly")
+            if is_unsafe or isinstance(types.get(member_type), (Handle, Function)):
+                modifiers.append("unsafe")
+            yield f"    {" ".join(modifiers)} {member_type} {member_name};\n"
+        yield "}\n"
+
+class StructMember:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.name: str = data["name"]
+        self.type: str = data["type"]
+        self.description: list[str] | None = data.get("description")
+
+class Function(Type):
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
+        self.arguments: list[FunctionArgument] = [
+            FunctionArgument(argument) for argument in data["arguments"]
+        ]
+        self.return_value: FunctionReturnValue | None = None
+        return_value: dict[str, Any] | None = data.get("return_value")
+        if return_value:
+            self.return_value = FunctionReturnValue(return_value)
+
+    def expand(self, types: dict[str, Type]) -> str:
+        type_parameters: list[str] = []
+        for argument in self.arguments:
+            argument_type, _, is_unsafe = resolve(argument.type)
+            split: int = len(argument_type) - is_unsafe
+            typedef: Type | None = types.get(argument_type[:split])
+            if typedef:
+                argument_type = typedef.expand(types) + argument_type[split:]
+            type_parameters.append(argument_type)
+        if self.return_value:
+            return_value_type, _, is_unsafe = resolve(self.return_value.type)
+            split: int = len(return_value_type) - is_unsafe
+            typedef: Type | None = types.get(return_value_type[:split])
+            if typedef:
+                return_value_type = typedef.expand(types) + return_value_type[split:]
+            type_parameters.append(return_value_type)
+        else:
+            type_parameters.append("void")
+        return f"delegate* unmanaged[Cdecl]<{", ".join(type_parameters)}>"
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        yield f"global using unsafe {self.name} = {self.expand(types)};\n"
+
+class FunctionReturnValue:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.type: str = data["type"]
+        self.description: list[str] | None = data.get("description")
+
+class FunctionArgument:
+    def __init__(self, data: dict[str, Any]) -> None:
+        self.type: str = data["type"]
+        self.name: str | None = data.get("name")
+        self.description: list[str] | None = data.get("description")
+
+class InterfaceFunction(Function):
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
+        self.since: str = data["since"]
+        self.see: list[str] | None = data.get("see")
+        self.legacy_type_name: str | None = data.get("legacy_type_name")
+
+    def generate(self, types: dict[str, Type]) -> Iterator[str]:
+        function_name: str = "GDExtensionInterface" + self.name.title().replace("_", "")
+        yield f"global using unsafe {function_name} = {self.expand(types)};\n"
+
+def resolve(typedef: str) -> tuple[str, bool, bool]:
+    is_readonly: bool = typedef.startswith("const")
+    is_unsafe: bool = typedef.endswith("*")
+    start: int = 6 if is_readonly else 0
+    end: int = -1 if is_unsafe else len(typedef)
+    name: str = typedef[start:end]
+    match name:
+        case "int8_t":
+            name = "sbyte"
+        case "uint8_t":
+            name = "byte"
+        case "int16_t":
+            name = "short"
+        case "uint16_t":
+            name = "ushort"
+        case "int32_t":
+            name = "int"
+        case "uint32_t":
+            name = "uint"
+        case "int64_t":
+            name = "long"
+        case "uint64_t":
+            name = "ulong"
+        case "size_t":
+            name = "nuint"
+        case "char":
+            name = "byte"
+        case "char16_t":
+            name = "ushort"
+        case "char32_t":
+            name = "uint"
+        case "wchar_t":
+            name = "char"
+    if is_unsafe:
+        name += "*"
+    return name, is_readonly, is_unsafe
