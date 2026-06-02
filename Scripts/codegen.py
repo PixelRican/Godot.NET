eaa702d6c31a1d9c958e826bcfb0d8e4775924a2
PixelRican <rimercado02@gmail.com>
2026-06-02 13:02:24 -0400

    Temporarily remove api.Extension construction

--- Scripts/codegen.py
+++ Scripts/codegen.py
@@ -1,10 +1,7 @@
-﻿import api
-import gdextension
+﻿import gdextension
 import json
 
 with open("gdextension_interface.json", "r") as file:
     interface = gdextension.Interface(json.load(file))
 with open("../Source/GDExtensionInterface.cs", "w") as file:
     file.writelines(interface.generate())
-with open("extension_api.json", "r") as file:
-    api = api.Extension(json.load(file))
