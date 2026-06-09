31b41051febfbb1094a4bd0630c0d4f478332eb5
PixelRican <rimercado02@gmail.com>
2026-06-09 15:26:46 -0400

    Add fields to GDExample class

--- Tests/GDExample.cs
+++ Tests/GDExample.cs
@@ -1,3 +1,7 @@
 ﻿namespace Godot.NET.Tests;
 
-public sealed class GDExample : GDExtensionObject;
+public sealed class GDExample : GDExtensionObject
+{
+    public double Amplitude = 10.0;
+    public double Speed = 1.0;
+}
