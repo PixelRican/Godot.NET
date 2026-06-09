26a25e8021fb6b4ad2848368108afa3aa08f047a
PixelRican <rimercado02@gmail.com>
2026-06-09 15:29:46 -0400

    Create GDExtensionString.cs

---
+++ Tests/GDExtensionString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Godot.NET.Tests;
+
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct GDExtensionString : IDisposable
+{
+    private void* _pointer;
+
+    public GDExtensionString(ReadOnlySpan<byte> value)
+    {
+        fixed (byte* reference = value)
+        fixed (void** pointer = &_pointer)
+        {
+            GDExtensionUninitializedStringPtr argument = new GDExtensionUninitializedStringPtr(pointer);
+            GDExtensionInterface.StringNewWithUtf8Chars(argument, reference);
+        }
+    }
+
+    public void Dispose()
+    {
+        fixed (void** pointer = &_pointer)
+        {
+            GDExtensionTypePtr argument = new GDExtensionTypePtr(pointer);
+            GDExtensionInterface.VariantGetPtrDestructor(GDExtensionVariantTypeString).Method(argument);
+        }
+    }
+}
