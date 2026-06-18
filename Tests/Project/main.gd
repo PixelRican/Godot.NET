a909584385461b462247ecda346243bd83d7d918
Roberto I. Mercado <rimercado02@gmail.com>
2026-06-18 10:43:53 -0400

    Add missing type annotations

--- Tests/Project/main.gd
+++ Tests/Project/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-func _ready():
+func _ready() -> void:
 	$GDExample.position_changed.connect(_on_position_changed)
 
-func _on_position_changed(new_position: Vector2):
+func _on_position_changed(new_position: Vector2) -> void:
 	prints("New position:", new_position)
