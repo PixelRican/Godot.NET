22aa4bfb904bba9544be13c0795d494cb4139f0b
PixelRican <rimercado02@gmail.com>
2026-07-09 17:19:23 -0400

    Move code body to main() function

--- Scripts/bindings_generator.py
+++ Scripts/bindings_generator.py
@@ -2,7 +2,11 @@
 from typing import Any
 import json
 
-with open("gdextension_interface.json", "r") as file:
-    data: dict[str, Any] = json.load(file)
-interface: GDExtensionInterface = GDExtensionInterface(data)
-interface.generate()
+def main() -> None:
+    with open("gdextension_interface.json", "r") as file:
+        data: dict[str, Any] = json.load(file)
+    interface: GDExtensionInterface = GDExtensionInterface(data)
+    interface.generate()
+
+if __name__ == "__main__":
+    main()
