e90916f49db398a5846116e7b6499ae3229c0640
PixelRican <rimercado02@gmail.com>
2026-07-25 19:34:30 -0400

    Generate using gdextension.py only (for now)

--- Scripts/generate.py
+++ Scripts/generate.py
@@ -1,13 +1,7 @@
 from gdextension import GDExtensionInterface
-from godot import GodotBindings
-from interop import InteropServices
 import json
 
 if __name__ == "__main__":
     with open("gdextension_interface.json", "r") as file:
-        gdextension: GDExtensionInterface = GDExtensionInterface(json.load(file))
-    with open("extension_api.json", "r") as file:
-        godot: GodotBindings = GodotBindings(json.load(file))
-    interop: InteropServices = InteropServices(gdextension, godot)
-    gdextension.generate()
-    interop.generate()
+        interface: GDExtensionInterface = GDExtensionInterface(json.load(file))
+    interface.generate()
