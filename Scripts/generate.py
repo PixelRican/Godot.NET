08355e885d9cb44245e4c2d97c7bdb8d1d70ecdb
PixelRican <rimercado02@gmail.com>
2026-07-25 19:36:14 -0400

    Minor refactor

--- Scripts/generate.py
+++ Scripts/generate.py
@@ -1,7 +1,7 @@
 from gdextension import GDExtensionInterface
-import json
+from json import load
 
 if __name__ == "__main__":
     with open("gdextension_interface.json", "r") as file:
-        interface: GDExtensionInterface = GDExtensionInterface(json.load(file))
+        interface: GDExtensionInterface = GDExtensionInterface(load(file))
     interface.generate()
