e63b24d388aaa36003b3631a06029ccba3bb535b
Roberto I. Mercado <rimercado02@gmail.com>
2026-08-12 17:54:04 -0400

    Move data files to folder

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,7 +4,7 @@ from typing import Any
 import gdextension
 
 if __name__ == "__main__":
-    with open("gdextension_interface.json", "r") as file:
+    with open("data/gdextension_interface.json", "r") as file:
         interop_data: dict[str, Any] = load(file)
     interop_generator: SourceGenerator = gdextension.parse(interop_data)
     interop_generator.generate()
