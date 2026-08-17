5f8279bf73bfb3b4e39e0d384a7307917f2e6d32
Roberto I. Mercado <rimercado02@gmail.com>
2026-08-17 17:57:58 -0400

    Return CSharpAttribute

--- Scripts/gdextension.py
+++ Scripts/gdextension.py
@@ -9,14 +9,14 @@ class GDExtensionDeprecated:
         self.__message: Optional[str] = data.get("message")
         self.__replace_with: Optional[str] = data.get("replace_with")
 
-    def to_csharp(self: Self, generator: SourceGenerator) -> str:
+    def to_csharp(self: Self, generator: SourceGenerator) -> CSharpAttribute:
         sentences: list[str] = [f"Deprecated since Godot {self.__since}."]
         if self.__message:
             sentences.append(self.__message)
         if self.__replace_with:
             sentences.append(f"Use `{generator.get_translation(self.__replace_with)}` instead.")
         argument: str = " ".join(sentences)
-        return f"Obsolete(\"{argument}\")"
+        return CSharpAttribute("Obsolete", [f"\"{argument}\""])
 
 
 def parse(data: dict[str, Any]) -> SourceGenerator:
