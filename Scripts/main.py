4ab93d27b64838a0af2f2edb76896a03ef3d3bc5
Roberto I. Mercado <rimercado02@gmail.com>
2026-08-18 18:56:34 -0400

    Update main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,10 +1,8 @@
-from csharp import SourceGenerator
+from gdextension import GDExtensionInterface
 from json import load
 from typing import Any
-import gdextension
 
 if __name__ == "__main__":
     with open("data/gdextension_interface.json", "r") as file:
         interop_data: dict[str, Any] = load(file)
-    interop_generator: SourceGenerator = gdextension.parse(interop_data)
-    interop_generator.generate()
+    GDExtensionInterface(interop_data).generate()
