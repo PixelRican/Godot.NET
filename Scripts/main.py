f99f2a27564fa4c6f968e05337cc828c295361f8
Roberto I. Mercado <rimercado02@gmail.com>
2026-08-19 12:59:26 -0400

    Add newline

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,6 +2,7 @@ from gdextension import GDExtensionInterface
 from json import load
 from typing import Any
 
+
 if __name__ == "__main__":
     with open("data/gdextension_interface.json", "r") as file:
         interop_data: dict[str, Any] = load(file)
