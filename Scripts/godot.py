2409313eca1b3a3efe7a510ed946775365787f13
Roberto I. Mercado <rimercado02@gmail.com>
2026-08-27 13:25:04 -0400

    Make class attributes private

--- Scripts/godot.py
+++ Scripts/godot.py
@@ -174,263 +174,263 @@ class GodotBuiltinClassMemberOffsetGrouping:
 
 class GodotBuiltinClassMemberOffsetRecord:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.member: str = data["member"]
-        self.offset: int = data["offset"]
-        self.meta: str = data["meta"]
+        self.__member: str = data["member"]
+        self.__offset: int = data["offset"]
+        self.__meta: str = data["meta"]
 
 
 class GodotGlobalEnum:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.is_bitfield: bool = data["is_bitfield"]
-        self.values: list[GodotGlobalEnumValue] = [
+        self.__name: str = data["name"]
+        self.__is_bitfield: bool = data["is_bitfield"]
+        self.__values: list[GodotGlobalEnumValue] = [
             GodotGlobalEnumValue(element) for element in data["values"]
         ]
 
 
 class GodotGlobalEnumValue:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.value: int = data["value"]
+        self.__name: str = data["name"]
+        self.__value: int = data["value"]
 
 
 class GodotUtilityFunction:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.return_type: Optional[str] = data.get("return_type")
-        self.category: str = data["category"]
-        self.is_vararg: bool = data["is_vararg"]
-        self.hash: int = data["hash"]
-        self.arguments: Optional[list[GodotUtilityFunctionArgument]] = None
+        self.__name: str = data["name"]
+        self.__return_type: Optional[str] = data.get("return_type")
+        self.__category: str = data["category"]
+        self.__is_vararg: bool = data["is_vararg"]
+        self.__hash: int = data["hash"]
+        self.__arguments: Optional[list[GodotUtilityFunctionArgument]] = None
         if arguments := data.get("arguments"):
-            self.arguments = [
+            self.__arguments = [
                 GodotUtilityFunctionArgument(element) for element in arguments
             ]
 
 
 class GodotUtilityFunctionArgument:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
 
 
 class GodotBuiltinClass:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.is_keyed: bool = data["is_keyed"]
-        self.operators: list[GodotBuiltinClassOperator] = [
+        self.__name: str = data["name"]
+        self.__is_keyed: bool = data["is_keyed"]
+        self.__operators: list[GodotBuiltinClassOperator] = [
             GodotBuiltinClassOperator(element) for element in data["operators"]
         ]
-        self.constructors: list[GodotBuiltinClassConstructor] = [
+        self.__constructors: list[GodotBuiltinClassConstructor] = [
             GodotBuiltinClassConstructor(element) for element in data["constructors"]
         ]
-        self.has_destructor: bool = data["has_destructor"]
-        self.indexing_return_type: Optional[str] = data.get("indexing_return_type")
-        self.methods: Optional[list[GodotBuiltinClassMethod]] = None
-        self.constants: Optional[list[GodotBuiltinClassConstant]] = None
-        self.enums: Optional[list[GodotBuiltinClassEnum]] = None
+        self.__has_destructor: bool = data["has_destructor"]
+        self.__indexing_return_type: Optional[str] = data.get("indexing_return_type")
+        self.__methods: Optional[list[GodotBuiltinClassMethod]] = None
+        self.__constants: Optional[list[GodotBuiltinClassConstant]] = None
+        self.__enums: Optional[list[GodotBuiltinClassEnum]] = None
         if methods := data.get("methods"):
-            self.methods = [
+            self.__methods = [
                 GodotBuiltinClassMethod(element) for element in methods
             ]
         if constants := data.get("constants"):
-            self.constants = [
+            self.__constants = [
                 GodotBuiltinClassConstant(element) for element in constants
             ]
         if enums := data.get("enums"):
-            self.enums = [
+            self.__enums = [
                 GodotBuiltinClassEnum(element) for element in enums
             ]
 
 
 class GodotBuiltinClassOperator:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.right_type: Optional[str] = data.get("right_type")
-        self.return_type: str = data["return_type"]
+        self.__name: str = data["name"]
+        self.__right_type: Optional[str] = data.get("right_type")
+        self.__return_type: str = data["return_type"]
 
 
 class GodotBuiltinClassConstructor:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.index: int = data["index"]
-        self.arguments: Optional[list[GodotBuiltinClassConstructorArgument]] = None
+        self.__index: int = data["index"]
+        self.__arguments: Optional[list[GodotBuiltinClassConstructorArgument]] = None
         if arguments := data.get("arguments"):
-            self.arguments = [
+            self.__arguments = [
                 GodotBuiltinClassConstructorArgument(element) for element in arguments
             ]
 
 
 class GodotBuiltinClassConstructorArgument:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
 
 
 class GodotBuiltinClassMethod:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.return_type: Optional[str] = data.get("return_type")
-        self.is_vararg: bool = data["is_vararg"]
-        self.is_const: bool = data["is_const"]
-        self.is_static: bool = data["is_static"]
-        self.hash: int = data["hash"]
-        self.arguments: Optional[list[GodotBuiltinClassMethodArgument]] = None
-        self.hash_compatibility: int = data["hash"]
+        self.__name: str = data["name"]
+        self.__return_type: Optional[str] = data.get("return_type")
+        self.__is_vararg: bool = data["is_vararg"]
+        self.__is_const: bool = data["is_const"]
+        self.__is_static: bool = data["is_static"]
+        self.__hash: int = data["hash"]
+        self.__arguments: Optional[list[GodotBuiltinClassMethodArgument]] = None
+        self.__hash_compatibility: int = data["hash"]
         if arguments := data.get("arguments"):
-            self.arguments = [
+            self.__arguments = [
                 GodotBuiltinClassMethodArgument(element) for element in arguments
             ]
 
 
 class GodotBuiltinClassMethodArgument:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
-        self.default_value: Union[str, float, int, bool, None] = data.get("default_value")
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
+        self.__default_value: Union[str, float, int, bool, None] = data.get("default_value")
 
 
 class GodotBuiltinClassConstant:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
-        self.value: str = data["value"]
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
+        self.__value: str = data["value"]
 
 
 class GodotBuiltinClassEnum:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.values: list[GodotBuiltinClassEnumValue] = [
+        self.__name: str = data["name"]
+        self.__values: list[GodotBuiltinClassEnumValue] = [
             GodotBuiltinClassEnumValue(element) for element in data["values"]
         ]
 
 
 class GodotBuiltinClassEnumValue:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.value: int = data["value"]
+        self.__name: str = data["name"]
+        self.__value: int = data["value"]
 
 
 class GodotClass:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.is_refcounted: bool = data["is_refcounted"]
-        self.is_instantiable: bool = data["is_instantiable"]
-        self.inherits: Optional[str] = data.get("inherits")
-        self.api_type: str = data["api_type"]
-        self.enums: Optional[list[GodotClassEnum]] = None
-        self.methods: Optional[list[GodotClassMethod]] = None
-        self.properties: Optional[list[GodotClassProperty]] = None
-        self.signals: Optional[list[GodotClassSignal]] = None
-        self.constants: Optional[list[GodotClassConstant]] = None
+        self.__name: str = data["name"]
+        self.__is_refcounted: bool = data["is_refcounted"]
+        self.__is_instantiable: bool = data["is_instantiable"]
+        self.__inherits: Optional[str] = data.get("inherits")
+        self.__api_type: str = data["api_type"]
+        self.__enums: Optional[list[GodotClassEnum]] = None
+        self.__methods: Optional[list[GodotClassMethod]] = None
+        self.__properties: Optional[list[GodotClassProperty]] = None
+        self.__signals: Optional[list[GodotClassSignal]] = None
+        self.__constants: Optional[list[GodotClassConstant]] = None
         if enums := data.get("enums"):
-            self.enums = [
+            self.__enums = [
                 GodotClassEnum(element) for element in enums
             ]
         if methods := data.get("methods"):
-            self.methods = [
+            self.__methods = [
                 GodotClassMethod(element) for element in methods
             ]
         if properties := data.get("properties"):
-            self.properties = [
+            self.__properties = [
                 GodotClassProperty(element) for element in properties
             ]
         if signals := data.get("signals"):
-            self.signals = [
+            self.__signals = [
                 GodotClassSignal(element) for element in signals
             ]
         if constants := data.get("constants"):
-            self.constants = [
+            self.__constants = [
                 GodotClassConstant(element) for element in constants
             ]
 
 
 class GodotClassEnum:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.values: list[GodotClassEnumValue] = [
+        self.__name: str = data["name"]
+        self.__values: list[GodotClassEnumValue] = [
             GodotClassEnumValue(element) for element in data["values"]
         ]
-        self.is_bitfield: bool = data["is_bitfield"]
+        self.__is_bitfield: bool = data["is_bitfield"]
 
 
 class GodotClassEnumValue:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.value: int = data["value"]
+        self.__name: str = data["name"]
+        self.__value: int = data["value"]
 
 
 class GodotClassMethod:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.is_const: bool = data["is_const"]
-        self.is_vararg: bool = data["is_vararg"]
-        self.is_static: bool = data["is_static"]
-        self.is_virtual: bool = data["is_virtual"]
-        self.hash: int = data["hash"]
-        self.hash_compatibility: Optional[list[int]] = data.get("hash_compatibility")
-        self.return_value: Optional[GodotClassMethodReturnValue] = None
-        self.arguments: Optional[list[GodotClassMethodArgument]] = None
-        self.is_required: Optional[bool] = data.get("is_required")
+        self.__name: str = data["name"]
+        self.__is_const: bool = data["is_const"]
+        self.__is_vararg: bool = data["is_vararg"]
+        self.__is_static: bool = data["is_static"]
+        self.__is_virtual: bool = data["is_virtual"]
+        self.__hash: int = data["hash"]
+        self.__hash_compatibility: Optional[list[int]] = data.get("hash_compatibility")
+        self.__return_value: Optional[GodotClassMethodReturnValue] = None
+        self.__arguments: Optional[list[GodotClassMethodArgument]] = None
+        self.__is_required: Optional[bool] = data.get("is_required")
         if return_value := data.get("return_value"):
-            self.return_value = GodotClassMethodReturnValue(return_value)
+            self.__return_value = GodotClassMethodReturnValue(return_value)
         if arguments := data.get("arguments"):
-            self.arguments = [
+            self.__arguments = [
                 GodotClassMethodArgument(element) for element in arguments
             ]
 
 
 class GodotClassMethodReturnValue:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.type: str = data["type"]
-        self.meta: Optional[str] = data.get("meta")
+        self.__type: str = data["type"]
+        self.__meta: Optional[str] = data.get("meta")
 
 
 class GodotClassMethodArgument:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
-        self.default_value: Union[str, float, int, bool, None] = data.get("default_value")
-        self.meta: Optional[str] = data.get("meta")
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
+        self.__default_value: Union[str, float, int, bool, None] = data.get("default_value")
+        self.__meta: Optional[str] = data.get("meta")
 
 
 class GodotClassProperty:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
-        self.setter: Optional[str] = data.get("setter")
-        self.getter: Optional[str] = data.get("getter")
-        self.index: Optional[int] = data.get("index")
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
+        self.__setter: Optional[str] = data.get("setter")
+        self.__getter: Optional[str] = data.get("getter")
+        self.__index: Optional[int] = data.get("index")
 
 
 class GodotClassSignal:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.arguments: Optional[list[GodotClassSignalArgument]] = None
+        self.__name: str = data["name"]
+        self.__arguments: Optional[list[GodotClassSignalArgument]] = None
         if arguments := data.get("arguments"):
-            self.arguments = [
+            self.__arguments = [
                 GodotClassSignalArgument(element) for element in arguments
             ]
 
 
 class GodotClassSignalArgument:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
 
 
 class GodotClassConstant:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.value: int = data["value"]
+        self.__name: str = data["name"]
+        self.__value: int = data["value"]
 
 
 class GodotSingleton:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.type: str = data["type"]
+        self.__name: str = data["name"]
+        self.__type: str = data["type"]
 
 
 class GodotNativeStructure:
     def __init__(self, data: dict[str, Any]) -> None:
-        self.name: str = data["name"]
-        self.format: str = data["format"]
+        self.__name: str = data["name"]
+        self.__format: str = data["format"]
